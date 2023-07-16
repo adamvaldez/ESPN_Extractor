6a711adcd6cd0d8f75d24ffc7f757732c1d9d6ee
Adam Valdez <7329668+adamvaldez@users.noreply.github.com>
2023-07-16 15:14:08 -0700

    Update post lint

--- tests/test_csv_extration.py
+++ tests/test_csv_extration.py
@@ -1,3 +1,4 @@
+""" Test Extraction of CSV File """
 import os
 import csv
 from unittest import TestCase
@@ -5,27 +6,31 @@ from espn_extractor.extract import extract_team_records
 
 
 class TestCsvExtraction(TestCase):
-
+    """ Class for CSV Extraction Test """
     def setUp(self) -> None:
         if os.path.isfile('tests/results/test_csv_file.csv'):
             os.remove('tests/results/test_csv_file.csv')
         extract_team_records('tests/results/test_csv_file.csv', ',', True)
 
     def test_csv_extraction(self):
+        """ Test if CSV file is created """
         self.assertTrue(os.path.isfile('tests/results/test_csv_file.csv'))
 
     def test_verify_csv_header(self):
-        header = ['owner', 'year', 'team_name', 'win', 'loss', 'draws', 'final_standing', 'points_for',
-                  'points_against', 'acquisitions', 'trades', 'drops', 'streak_length', 'streak_type', 'playoff_seed']
-        with open('tests/results/test_csv_file.csv', newline='') as f:
-            reader = csv.reader(f)
+        """ Verify the header of CSV test file """
+        header = ['owner', 'year', 'team_name', 'win', 'loss', 'draws', 'final_standing',
+                  'points_for', 'points_against', 'acquisitions', 'trades', 'drops',
+                  'streak_length', 'streak_type', 'playoff_seed']
+        with open('tests/results/test_csv_file.csv', newline='', encoding="utf8") as file:
+            reader = csv.reader(file)
             self.assertEqual(header, next(reader))
 
     def test_verify_csv_first_row(self):
-        user_date = ['jessie marshall', '2018', 'Team 1', '10', '3', '0', '4', '1276.88', '1038.22', '21', '1',
-                     '22', '1', 'WIN', '2']
-        with open('tests/results/test_csv_file.csv', newline='\n') as f:
-            reader = csv.reader(f)
+        """ Verify first line of CSV test file """
+        user_date = ['jessie marshall', '2018', 'Team 1', '10', '3', '0', '4', '1276.88',
+                     '1038.22', '21', '1', '22', '1', 'WIN', '2']
+        with open('tests/results/test_csv_file.csv', newline='\n', encoding="utf8") as file:
+            reader = csv.reader(file)
             # Skip Header
             next(reader)
             # When reading, remove trailing white space
