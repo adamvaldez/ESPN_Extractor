7ab9aead69c581444f66a2de4a43600c6d13ce62
Adam Valdez <7329668+adamvaldez@users.noreply.github.com>
2023-07-23 18:16:07 -0700

    Fixed lint issue, ignore xlsx results

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 """ Run ESPN Fantasy Data Extractor """
 from espn_extractor.league_history import extract_team_records
-from espn_extractor.draft_board import extract_draft_board,
-                                       extract_draft_positions
-from espn_extractor.draft_board_excel import extract_draft_positions
+from espn_extractor.draft_board import extract_draft_board, extract_draft_positions
+from espn_extractor.draft_board_excel import extract_draft_cheatsheet
 import config
 
 if __name__ == '__main__':
     extract_team_records(config, False)
     extract_draft_board(config, False)
     extract_draft_positions(config, False)
-    extract_draft_positions(config, False)
+    extract_draft_cheatsheet(config, False)
